a93962b8ac7b31976e9a03cdf61fec855c34b548
seanbabby <seanbabby@gmail.com>
2017-06-07 15:35:42 +0800

    init project

---
+++ RoadChecKReport/LogInViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LogInViewController.swift
+//  RoadChecKReport
+//
+//  Created by Chang sean on 2017/6/7.
+//  Copyright © 2017年 Chang sean. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.blue
+    }
+
+    
+
+}
